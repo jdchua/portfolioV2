5b35a9c974829656ea2a262cda308949fc356e02
Jeremiah Chua <jeremiahdanielchua@gmail.com>
2019-06-29 04:04:57 +0000

    add files

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express"),
+    app     = express();
+    
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Portfolio is running!");
+})
